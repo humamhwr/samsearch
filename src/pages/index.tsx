4fe85c12d0b0f316af3adb716700477a504a9344
Humam Hawara <himoo1999@gmail.com>
2023-11-06 21:01:26 -0500

    removed pricing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Header from '../components/Header';
 import LazyShow from '../components/LazyShow';
 import MainHero from '../components/MainHero';
 import MainHeroImage from '../components/MainHeroImage';
-import Pricing from '../components/Pricing';
+// import Pricing from '../components/Pricing';
 import Product from '../components/Product';
 
 const App = () => {
@@ -38,12 +38,10 @@ const App = () => {
           <Canvas />
         </>
       </LazyShow>
-      <LazyShow>
-        <Pricing />
-      </LazyShow>
+      
       <LazyShow>
         <>
-          <Canvas />
+          
           <About />
         </>
       </LazyShow>
